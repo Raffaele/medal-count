2e2a90d0b75e24da766447187fa020bb0ddfce93
Raffaele <mori.raffaele@gmail.com>
2025-05-30 19:34:00 +0100

    Setup for loading and error

--- src/app/_components/MedalTable/MedalTable.tsx
+++ src/app/_components/MedalTable/MedalTable.tsx
@@ -1,7 +1,55 @@
-import { SortKey } from "@/types";
+"use client";
+
+import { CountryResult, SortKey } from "@/types";
+import { useQuery } from "@tanstack/react-query";
+import style from "./MedalTable.module.css";
 
 type Props = { sort: SortKey };
 
 export const MedalTable = ({ sort }: Props) => {
-  return <div>MedalTable</div>;
+  const {
+    isLoading,
+    isError,
+    data: countries = [],
+    refetch,
+  } = useQuery<CountryResult[]>({
+    queryKey: ["medals"],
+    queryFn: async () => {
+      const res = await fetch("/api/medals");
+      if (!res.ok) throw new Error(res.statusText);
+      return res.json();
+    },
+    retry: 0,
+    select: (data) => {
+      const sortedCodes = data.map(({ code }) => code);
+      return data.map((country) => {
+        return {
+          ...country,
+          total: country.gold + country.silver + country.bronze,
+          flagIndex: sortedCodes.indexOf(country.code),
+        };
+      });
+    },
+  });
+
+  if (isLoading) return <div className={style.loadingPanel}>Loading...</div>;
+
+  if (isError)
+    return (
+      <div className={style.errorPanel}>
+        <h3>Error</h3>
+        <button onClick={() => refetch()}>RETRY</button>
+      </div>
+    );
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th></th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody></tbody>
+    </table>
+  );
 };
